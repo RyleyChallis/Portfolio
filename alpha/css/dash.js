6b4d141ebd8728880fdcaa7d6e0e02b8adeec190
Ryley C <ryleychallis15@gmail.com>
2026-05-12 07:49:53 +0100

    Upload files

--- alpha/css/dash.js
+++ alpha/css/dash.js
@@ -1,29 +1,29 @@
-document.documentElement.style.display = 'none';
+// document.documentElement.style.display = 'none';
 
-    async function validateAccess() {
-        const secretHash = "5eeb0e39b3d3e094d8bb00b2c96dabedda316eb4d6f0e7fd3198106901820e94";
+//     async function validateAccess() {
+//         const secretHash = "5eeb0e39b3d3e094d8bb00b2c96dabedda316eb4d6f0e7fd3198106901820e94";
         
-        const entry = prompt("RC | Client Portal - Please enter your access code:");
+//         const entry = prompt("RC | Client Portal - Please enter your access code:");
         
-        if (!entry) {
-            window.location.href = "https://google.com";
-            return;
-        }
+//         if (!entry) {
+//             window.location.href = "https://google.com";
+//             return;
+//         }
 
-        const msgBuffer = new TextEncoder().encode(entry);
-        const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-        const hashArray = Array.from(new Uint8Array(hashBuffer));
-        const hashedEntry = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+//         const msgBuffer = new TextEncoder().encode(entry);
+//         const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+//         const hashArray = Array.from(new Uint8Array(hashBuffer));
+//         const hashedEntry = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
 
-        if (hashedEntry === secretHash) {
-        document.documentElement.style.display = 'block';
-        } else {
-        alert("Incorrect code.");
-        window.location.href = "https://google.com";
-        }
-    }
+//         if (hashedEntry === secretHash) {
+//         document.documentElement.style.display = 'block';
+//         } else {
+//         alert("Incorrect code.");
+//         window.location.href = "https://google.com";
+//         }
+//     }
 
-    window.onload = validateAccess;
+//     window.onload = validateAccess;
 
 
 const currentStageNumber = 1; 
